40b5767cecad10b6751a2298463c709bbbf5612b
CompanyProfile <alanood94895@gmail.com>
2026-05-10 12:25:31 +0400

    Add Appointment class

--- src/Entities/Appointment.java
+++ src/Entities/Appointment.java
@@ -3,4 +3,61 @@ import java.time.LocalDate;
 
 
 public class Appointment {
+
+    private String appointmentId;
+    private String patientId;
+    private String doctorId;
+    private LocalDate appointmentDate;
+    private String appointmentTime;
+    private String status;
+    private String reason;
+    private String notes;
+
+    public Appointment(String appointmentId, String patientId, String doctorId, LocalDate appointmentDate,
+                       String appointmentTime, String status, String reason, String notes) {
+
+
+        this.appointmentId = appointmentId;
+        this.patientId = patientId;
+        this.doctorId = doctorId;
+        this.appointmentDate = appointmentDate;
+        this.appointmentTime = appointmentTime;
+        this.status = status;
+        this.reason = reason;
+        this.notes = notes;
+    }
+
+    public void reschedule(LocalDate newDate, String newTime) {
+        this.appointmentDate = newDate;
+        this.appointmentTime = newTime;
+        status = "Rescheduled";
+    }
+
+    public void cancel() {
+        status = "Cancelled";
+    }
+
+    public void complete() {
+        status = "Completed";
+    }
+
+    public void displayInfo() {
+        System.out.println("Appointment ID: " + appointmentId);
+        System.out.println("Status: " + status);
+    }
+
+    public String getPatientId() {
+        return patientId;
+    }
+
+    public String getDoctorId() {
+        return doctorId;
+    }
+
+    public void  assignDoctor() {
+
+    }
+
+
 }
+
