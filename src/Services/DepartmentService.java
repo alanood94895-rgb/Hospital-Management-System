7f8bb4769ce773f56dbce8c5363c4e874ca38425
CompanyProfile <alanood94895@gmail.com>
2026-05-10 14:07:49 +0400

    Add DepartmentService Class

--- src/Services/DepartmentService.java
+++ src/Services/DepartmentService.java
@@ -5,5 +5,10 @@ public class DepartmentService {
     private List<Department> departments =
             new ArrayList<>();
 
+    // Add Department
+    public void addDepartment(Department department) {
+        departments.add(department);
+    }
+
 
 }
