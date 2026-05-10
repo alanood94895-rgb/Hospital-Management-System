9832087ff6df392d555913de09cc977eda7916cd
CompanyProfile <alanood94895@gmail.com>
2026-05-10 13:33:58 +0400

    Add DoctorService Class

--- src/Services/DoctorService.java
+++ src/Services/DoctorService.java
@@ -5,4 +5,17 @@ import java.util.List;
 
 
 public class DoctorService {
+    List<Doctor> doctors = new ArrayList<>();
+
+    public void addDoctor(Doctor doctor) {
+        doctors.add(doctor);
+    }
+
+    public void displayAllDoctors() {
+
+        for (Doctor doctor : doctors) {
+            doctor.displayInfo();
+        }
+    }
+
 }
