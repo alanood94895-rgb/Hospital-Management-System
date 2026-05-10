348ba3b15d04baebce2ba329eb8faa5652cd3ce8
CompanyProfile <alanood94895@gmail.com>
2026-05-10 14:31:15 +0400

    Add Person Class

--- src/Entities/Person.java
+++ src/Entities/Person.java
@@ -32,6 +32,68 @@ public class Person {
 
         //get and set
 
+    public String getId() {
+            return id;
+        }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 
         public void displayInfo() {
             System.out.println("ID: " + id);
@@ -43,15 +105,10 @@ public class Person {
             System.out.println("Address: " + address);
         }
 
-        // Getters and Setters
 
-    public String getId() {
-        return id;
-    }
 
-    public void setId(String id) {
-        this.id = id;
-    }
+
+
 
     @Override
     public String toString() {
