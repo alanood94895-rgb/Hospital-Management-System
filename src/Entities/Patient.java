2415b86ff3ba00fb5d0dcf314664c25fa66f48d8
CompanyProfile <alanood94895@gmail.com>
2026-05-10 11:05:19 +0400

    Add Patient class

--- src/Entities/Patient.java
+++ src/Entities/Patient.java
@@ -1,4 +1,60 @@
 package Entities;
 
-public class Patient {
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Patient extends Person {
+
+    private String patientId;
+    private String bloodGroup;
+    private List<String> allergies;
+    private String emergencyContact;
+    private LocalDate registrationDate;
+    private String insuranceId;
+    private List<String> medicalRecords;
+    private List<String> appointments;
+
+    public Patient(String id, String firstName, String lastName,
+                   LocalDate dateOfBirth, String gender,
+                   String phoneNumber, String email, String address,
+                   String patientId, String bloodGroup,
+                   String emergencyContact, LocalDate registrationDate,
+                   String insuranceId) {
+
+        super(id, firstName, lastName, dateOfBirth,
+                gender, phoneNumber, email, address);
+
+        this.patientId = patientId;
+        this.bloodGroup = bloodGroup;
+        this.allergies = allergies;
+        this.emergencyContact = emergencyContact;
+        this.registrationDate = registrationDate;
+        this.insuranceId = insuranceId;
+        this. medicalRecords = medicalRecords ;
+        this. appointments = appointments ;
+
+        allergies = new ArrayList<>();
+    }
+
+    @Override
+    public void displayInfo() {
+
+        super.displayInfo();
+
+        System.out.println("Patient ID: " + patientId);
+        System.out.println("Blood Group: " + bloodGroup);
+        System.out.println("Insurance ID: " + insuranceId);
+    }
+
+    public void addAllergy(String allergy) {
+        allergies.add(allergy);
+    }
+
+    public void updateInsurance(String insuranceId) {
+        this.insuranceId = insuranceId;
+    }
 }
+
+
