8590836a9b99ef3734d099c1e618e58389e4c241
CompanyProfile <alanood94895@gmail.com>
2026-05-10 12:15:01 +0400

    Add Patient class

--- src/Entities/Patient.java
+++ src/Entities/Patient.java
@@ -13,58 +13,52 @@ public class Patient extends Person {
     private String emergencyContact;
     private LocalDate registrationDate;
     private String insuranceId;
-    private List<String> medicalRecords;
-    private List<String> appointments;
+
+    private List<MedicalRecord> medicalRecords;
+    private List<Appointment> appointments;
 
     public Patient(String id, String firstName, String lastName,
-                   LocalDate dateOfBirth, String gender,
-                   String phoneNumber, String email, String address,
+                   LocalDate dob, String gender,
+                   String phone, String email, String address,
                    String patientId, String bloodGroup,
-                   String emergencyContact, LocalDate registrationDate,
+                   String emergencyContact,
+                   LocalDate registrationDate,
                    String insuranceId) {
 
-        super(id, firstName, lastName, dateOfBirth,
-                gender, phoneNumber, email, address);
+        super(id, firstName, lastName, dob,
+                gender, phone, email, address);
 
         this.patientId = patientId;
         this.bloodGroup = bloodGroup;
-        this.allergies = allergies;
         this.emergencyContact = emergencyContact;
         this.registrationDate = registrationDate;
         this.insuranceId = insuranceId;
-        this. medicalRecords = medicalRecords ;
-        this. appointments = appointments ;
 
         allergies = new ArrayList<>();
+        medicalRecords = new ArrayList<>();
+        appointments = new ArrayList<>();
     }
 
     @Override
     public void displayInfo() {
-
         super.displayInfo();
-
         System.out.println("Patient ID: " + patientId);
         System.out.println("Blood Group: " + bloodGroup);
-        System.out.println("Insurance ID: " + insuranceId);
-    }
-    public void addMedicalRecord() {
-
     }
 
-    public void  addAppointment() {
-
-    }
-    public void  updateInsurance() {
-
+    public void addMedicalRecord(MedicalRecord record) {
+        medicalRecords.add(record);
     }
 
-    public void addAllergy(String allergy) {
-        allergies.add(allergy);
+    public void addAppointment(Appointment appointment) {
+        appointments.add(appointment);
     }
 
     public void updateInsurance(String insuranceId) {
         this.insuranceId = insuranceId;
     }
+
+    public String getPatientId() {
+        return patientId;
+    }
 }
-
-
