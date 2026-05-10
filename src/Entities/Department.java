501cd321197aa40e8b54248cc53cd5d9abc52e5a
CompanyProfile <alanood94895@gmail.com>
2026-05-10 12:28:34 +0400

    Add Department class

--- src/Entities/Department.java
+++ src/Entities/Department.java
@@ -4,4 +4,14 @@ import java.util.List;
 
 
 public class Department {
+
+    private String departmentId;
+    private String departmentName;
+    private String headDoctorId;
+
+    private List<Doctor> doctors;
+    private List<Nurse> nurses;
+
+    private int bedCapacity;
+    private int availableBeds;
 }
