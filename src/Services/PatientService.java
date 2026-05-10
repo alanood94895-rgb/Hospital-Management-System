13c60bb05f4a6ef829a3f2fa562f70806e45031e
CompanyProfile <alanood94895@gmail.com>
2026-05-10 12:34:47 +0400

    Add PatientService Class

--- src/Services/PatientService.java
+++ src/Services/PatientService.java
@@ -4,4 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PatientService {
+    static List<Patient> patients = new ArrayList<>();
+
+    public void addPatient(Patient patient) {
+        patients.add(patient);
+    }
+
+
 }
