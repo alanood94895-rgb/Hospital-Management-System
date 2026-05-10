67b0383f6c6cae79f1f8f1ffa718d3a7c1176056
CompanyProfile <alanood94895@gmail.com>
2026-05-10 14:58:05 +0400

    Add Patient Class

--- src/Entities/Patient.java
+++ src/Entities/Patient.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public class Patient extends Person {
 
-    // Patient Specific Fields
+    // Add Specific Fields
     private String patientId;
     private String bloodGroup;
     private List<String> allergies;
