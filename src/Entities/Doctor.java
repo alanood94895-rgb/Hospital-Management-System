f153ebd028109de7ea0ca97e770ba3ed7eef9206
CompanyProfile <alanood94895@gmail.com>
2026-05-11 10:54:59 +0400

    Add Doctor Class

--- src/Entities/Doctor.java
+++ src/Entities/Doctor.java
@@ -33,8 +33,8 @@ public class Doctor extends Person{
 
         super(id,
                 firstName,
-                dateOfBirth,
                 lastName,
+                dateOfBirth,
                 gender,
                 phoneNumber,
                 email,
