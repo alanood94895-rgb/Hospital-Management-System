79bc4a45945744fc8ee7ac0cde9a37443df2eb3b
CompanyProfile <alanood94895@gmail.com>
2026-05-11 14:05:49 +0400

    Add Consultant Class

---
+++ src/Entities/Consultant.java
@@ -0,0 +1,69 @@
+package Entities;
+
+import Entities.Doctor;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Consultant extends Doctor {
+
+    private List<String> consultationTypes;
+    Boolean onlineConsultationAvailable;
+    Integer consultationDuration;
+
+
+    public Consultant(String id,
+                      String firstName,
+                      String lastName,
+                      LocalDate dateOfBirth,
+                      String gender,
+                      String phoneNumber,
+                      String email,
+                      String address,
+                      String specialization,
+                      String qualification,
+                      Integer experienceYears,
+                      String departmentId,
+                      double consultationFee) {
+
+
+        super(id,
+                firstName,
+                lastName,
+                dateOfBirth,
+                gender,
+                phoneNumber,
+                email,
+                address,
+                specialization,
+                qualification,
+                experienceYears,
+                departmentId,
+                consultationFee);
+
+        this.consultationTypes = new ArrayList<>();
+    }
+
+    @Override
+    public void displayInfo() {
+        super.displayInfo();
+        System.out.println("Online Consultation Available: " + onlineConsultationAvailable);
+        System.out.println("Consultation Duration: " + consultationDuration);
+        System.out.println("Consultation Types");
+        for(String x : consultationTypes) {
+            System.out.print(x + " , ");
+        }
+    }
+
+    public void scheduleConsultation(String patientId, String type) {
+        System.out.println("Consultation scheduled for patient: " + patientId);
+        System.out.println("Type: " + type);
+        System.out.println("Duration: " + consultationDuration + " minutes");
+    }
+
+    public void provideSecondOpinion(){
+
+
+    }
+}
