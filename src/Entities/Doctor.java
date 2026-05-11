2f32322b2e2288026c1777a40689830b7dc9f56d
CompanyProfile <alanood94895@gmail.com>
2026-05-11 14:03:03 +0400

    Add Surgeon Class

--- src/Entities/Doctor.java
+++ src/Entities/Doctor.java
@@ -1,10 +1,7 @@
 package Entities;
-import Utils.Constants;
-
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 public class Doctor extends Person{
 
@@ -153,82 +150,5 @@ public class Doctor extends Person{
     public void updateAvailability(List<String> newSlots) {
         availableSlots = newSlots;
         System.out.println("Availability updated.");
-    }
-
-    public static class Surgeon extends Doctor {
-        static Scanner scanner = new Scanner(System.in);
-        private Integer surgeriesPerformed;
-        private List<String> surgeryTypes;
-        private Boolean operationTheatreAccess;
-
-
-        public Surgeon(String id,
-                       String firstName,
-                       String lastName,
-                       LocalDate dateOfBirth,
-                       String gender,
-                       String phoneNumber,
-                       String email,
-                       String address,
-                       String specialization,
-                       String qualification,
-                       Integer experienceYears,
-                       String departmentId,
-                       double consultationFee ,
-                       Integer surgeriesPerformed ,
-                       Boolean operationTheatreAccess) {
-
-
-            super( id,
-                    firstName,
-                    lastName,
-                    dateOfBirth,
-                    gender,
-                    phoneNumber,
-                    email,
-                    address,
-                    id,
-                    specialization,
-                    qualification,
-                    experienceYears,
-                    departmentId,
-                    consultationFee,
-                    new ArrayList<>(),
-                    new ArrayList<>()
-            );
-
-
-            this.surgeriesPerformed = surgeriesPerformed;
-            this.operationTheatreAccess = operationTheatreAccess;
-            this.surgeryTypes = new ArrayList<>();
-        }
-
-        @Override
-        public void displayInfo() {
-            super.displayInfo();
-            System.out.println("Surgeries Performed: " + surgeriesPerformed);
-            System.out.println("Operation Theatre Access: " + operationTheatreAccess);
-            for(String x : surgeryTypes){
-                System.out.print(x + " , ");
-            }
-        }
-        public void updateSurgeryCount(){
-            surgeriesPerformed = surgeriesPerformed + 1 ;
-        }
-
-        public void performSurgery(){
-            if(operationTheatreAccess){
-                System.out.println(Constants.SURGEON_CAN_OPERATE);
-                System.out.println("Enter surgery type: ");
-                surgeryTypes.add(scanner.nextLine());
-                updateSurgeryCount();
-
-            }else{
-                System.out.println(Constants.SURGEON_CANNOT_OPERATE);
-            }
-
-
-
-        }
     }
 }
---
+++ src/Entities/Surgeon.java
@@ -0,0 +1,86 @@
+package Entities;
+
+import Entities.Doctor;
+import Utils.Constants;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Surgeon extends Doctor {
+    static Scanner scanner = new Scanner(System.in);
+    private Integer surgeriesPerformed;
+    private List<String> surgeryTypes;
+    private Boolean operationTheatreAccess;
+
+
+    public Surgeon(String id,
+                   String firstName,
+                   String lastName,
+                   LocalDate dateOfBirth,
+                   String gender,
+                   String phoneNumber,
+                   String email,
+                   String address,
+                   String specialization,
+                   String qualification,
+                   Integer experienceYears,
+                   String departmentId,
+                   double consultationFee ,
+                   Integer surgeriesPerformed ,
+                   Boolean operationTheatreAccess) {
+
+
+        super( id,
+                firstName,
+                lastName,
+                dateOfBirth,
+                gender,
+                phoneNumber,
+                email,
+                address,
+                id,
+                specialization,
+                qualification,
+                experienceYears,
+                departmentId,
+                consultationFee,
+                new ArrayList<>(),
+                new ArrayList<>()
+        );
+
+
+        this.surgeriesPerformed = surgeriesPerformed;
+        this.operationTheatreAccess = operationTheatreAccess;
+        this.surgeryTypes = new ArrayList<>();
+    }
+
+    @Override
+    public void displayInfo() {
+        super.displayInfo();
+        System.out.println("Surgeries Performed: " + surgeriesPerformed);
+        System.out.println("Operation Theatre Access: " + operationTheatreAccess);
+        for(String x : surgeryTypes){
+            System.out.print(x + " , ");
+        }
+    }
+    public void updateSurgeryCount(){
+        surgeriesPerformed = surgeriesPerformed + 1 ;
+    }
+
+    public void performSurgery(){
+        if(operationTheatreAccess){
+            System.out.println(Constants.SURGEON_CAN_OPERATE);
+            System.out.println("Enter surgery type: ");
+            surgeryTypes.add(scanner.nextLine());
+            updateSurgeryCount();
+
+        }else{
+            System.out.println(Constants.SURGEON_CANNOT_OPERATE);
+        }
+
+
+
+    }
+}
