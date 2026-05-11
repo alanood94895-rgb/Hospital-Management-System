0eb9f70e870d243b4342e52489d5aa18a0c955af
CompanyProfile <alanood94895@gmail.com>
2026-05-11 13:38:34 +0400

    Add Menu Messages

---
+++ src/Utils/MenuMessages.java
@@ -0,0 +1,65 @@
+package Utils;
+
+public class MenuMessages {
+
+    public static String MAIN_MENU_MESSAGE = """
+            1- Patient Mene
+            2- Exit
+            """;
+    public static String PATIENT_MENU_MESSAGE = """
+            1- Add New Patient
+            2- Edit Patient
+            3- Remove Patient
+            4- Patient By Id
+            5- Patient By Name
+            6- All Patient
+            7- Exit
+            """;
+
+    public static String DEPARTMENT_MENU_MESSAGE = """
+            1- Add New department
+            2- Edit department
+            3- Remove department
+            4- department By Id
+            5- department By Name
+            6- All department
+            7- Exit
+            """;
+    public static String DOCTOR_MENU_MESSAGE = """
+            1- Add New doctor
+            2- Edit doctor
+            3- Remove doctor
+            4- doctor By Id
+            5- doctor By Name
+            6- All doctor
+            7- Exit
+            """;
+    public static String NURSE_MENU_MESSAGE = """
+            1- Add New nurse
+            2- Edit nurse
+            3- Remove nurse
+            4- nurse By Id
+            5- nurse By Name
+            6- All nurse
+            7- Exit
+            """;
+    public static String MEDICAL_RECORD_MENU_MESSAGE = """
+            1- Add New medical record
+            2- Edit medical record
+            3- Remove medical record
+            4- medical record By Id
+            5- medical record By Name
+            6- All medical record
+            7- Exit
+            """;
+    public static String APPOINTMENT_MENU_MESSAGE = """
+            1- Add New appointment
+            2- Edit appointment
+            3- Remove appointment
+            4- appointment By Id
+            5- appointment By Name
+            6- All appointment
+            7- Exit
+            """;
+}
+
