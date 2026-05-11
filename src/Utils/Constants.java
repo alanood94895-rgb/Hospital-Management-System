071279bf26adf3ff4614645e8bff2240453f4cdf
CompanyProfile <alanood94895@gmail.com>
2026-05-11 11:43:05 +0400

    Add new Constants

--- src/Utils/Constants.java
+++ src/Utils/Constants.java
@@ -2,14 +2,51 @@ package Utils;
 
 public class Constants {
 
-        public static String INSURANCE_UPDATE_SUCCESSFULLY ="Insurance has been updated ";
-        public static String PATIENT_ASSIGN_SUCCESSFULLY ="patient has been added ";
-        public static String REMOVE_PATIENT_SUCCESSFULLY ="patient has been removed ";
-        public static String NOT_RESCHEDULE =" Cannot reschedule a appointment  ";
-        public static String RESCHEDULE_SUCCESSFULLY ="reschedule appointment has been updated  ";
-        public static String CANCELLED ="appointment has been Cancelled ";
-        public static String COMPLETED ="appointment MARKED AS COMPLETED ";
-        public static String PATIENT_UPDATED =" Patient has been  updated successfully ";
+        public static String ADD_MEDICAL_RECORD = "Add Medical Record: ";
+        public static String ADD_APPOINTMENT = "Add Appointment: ";
+        public static String ENTER_INSURANCE = "Enter new Insurance: ";
+        public static String ASSIGNED = "Patient assigned.";
+        public static String PATIENT_REMOVED_SUCCESSFULLY = "Patient removed successfully";
+        public static String PATIENT_ADDED_SUCCESSFULLY = "Patient added successfully";
+        public static String AVAILABILITY_UPDATED = "Availability updated";
+        public static String PATIENT_UPDATED_SUCCESSFULLY = "Patient updated successfully";
+        public static String PATIENT_NOT_FOUND = "Patient not found";
 
-    }
+        public static String DOCTOR_ADDED_SUCCESSFULLY = "Doctor added successfully";
+        public static String DOCTOR_UPDATED_SUCCESSFULLY = "Doctor updated successfully";
+        public static String DOCTOR_NOT_FOUND = "Doctor not found";
+        public static String DOCTOR_REMOVED_SUCCESSFULLY = "Doctor removed successfully";
+
+        public static String NURSE_ADDED_SUCCESSFULLY = "Nurse added successfully";
+        public static String NURSE_UPDATED_SUCCESSFULLY = "Nurse updated successfully";
+        public static String NURSE_NOT_FOUND = "Nurse not found";
+        public static String NURSE_REMOVED_SUCCESSFULLY = "Nurse removed successfully";
+
+
+        public static String RECORD_ADDED_SUCCESSFULLY = "Medical record added successfully";
+        public static String RECORD_UPDATED_SUCCESSFULLY = "Medical record successfully";
+        public static String RECORD_NOT_FOUND = "Medical record not found";
+        public static String RECORD_REMOVED_SUCCESSFULLY = "Medical record removed successfully";
+
+        public static String APPOINTMENT_ADDED_SUCCESSFULLY = "Appointment added successfully";
+        public static String APPOINTMENT_UPDATED_SUCCESSFULLY = "Appointment successfully";
+        public static String APPOINTMENT_NOT_FOUND = "Appointment not found";
+        public static String APPOINTMENT_REMOVED_SUCCESSFULLY = "Appointment removed successfully";
+        public static String APPOINTMENT_RESCHEDULED_SUCCESSFULLY = "Appointment rescheduled successfully";
+        public static String APPOINTMENT_CANCELLED_SUCCESSFULLY = "Appointment cancelled successfully";
+
+
+        public static String DEPARTMENT_ADDED_SUCCESSFULLY = "Department added successfully";
+        public static String DEPARTMENT_UPDATED_SUCCESSFULLY = "Department successfully";
+        public static String DEPARTMENT_NOT_FOUND = "Department not found";
+        public static String DEPARTMENT_REMOVED_SUCCESSFULLY = "Department removed successfully";
+        public static String DEPARTMENT_RESCHEDULED_SUCCESSFULLY = "Department rescheduled successfully";
+        public static String DEPARTMENT_CANCELLED_SUCCESSFULLY = "Department cancelled successfully";
+
+        public static String SURGEON_CAN_OPERATE = "surgeon can operate";
+        public static String SURGEON_CANNOT_OPERATE = "surgeon can't operate";
+
+
+}
+
 
