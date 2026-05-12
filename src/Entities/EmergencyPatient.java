5b930fc55832f1ef81c682bce5b53c53fcfe8367
CompanyProfile <alanood94895@gmail.com>
2026-05-12 12:16:51 +0400

    Add inPatient Class

--- src/Entities/EmergencyPatient.java
+++ src/Entities/EmergencyPatient.java
@@ -1,35 +1,34 @@
 package Entities;
 
-import Entities.Patient;
+import Behaviour.Billable;
 import Behaviour.Displayable;
 
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 
-public class EmergencyPatient extends InPatient implements Displayable {
+public class inPatient extends Patient implements Displayable {
+    LocalDate admissionDate;
+    LocalDate dischargeDate;
+    String roomNumber;
+    String bedNumber;
+    String admittingDoctorId;
+    Double dailyCharges;
 
-    String emergencyType;
-    String arrivalMode;
-    Integer triageLevel;
-    Boolean admittedViaER;
 
-    public EmergencyPatient(String id,
-                            String firstName,
-                            String lastName,
-                            LocalDate dateOfBirth,
-                            String gender,
-                            String phoneNumber,
-                            String email,
-                            String address,
-                            String bloodGroup,
-                            String emergencyContact,
-                            LocalDate registrationDate,
-                            String insuranceId,
-                            List<String> allergies,
-                            String emergencyType ,
-                            String arrivalMode ,
-                            Integer triageLevel ,
-                            Boolean admittedViaER) {
+    public inPatient(String id,
+                     String firstName,
+                     String lastName,
+                     LocalDate dateOfBirth,
+                     String gender,
+                     String phoneNumber,
+                     String email,
+                     String address,
+                     String bloodGroup,
+                     String emergencyContact,
+                     LocalDate registrationDate,
+                     String insuranceId,
+                     List<String> allergies ) {
 
         super(id,
                 firstName,
@@ -45,26 +44,51 @@ public class EmergencyPatient extends InPatient implements Displayable {
                 insuranceId,
                 allergies);
 
-        this.emergencyType = emergencyType;
-        this.arrivalMode = arrivalMode;
-        this.triageLevel = triageLevel;
-        this.admittedViaER = admittedViaER;
+        this.admissionDate = admissionDate;
+        this.dischargeDate = dischargeDate;
+        this.roomNumber =  roomNumber;
+        this.bedNumber =  bedNumber;
+        this.admittingDoctorId  = admittingDoctorId;
+        this.dailyCharges = dailyCharges;
     }
-
 
     @Override
     public void displayInfo() {
         super.displayInfo();
-
-        System.out.println("Emergency Type: " + emergencyType);
-        System.out.println("Arrival Mode: " + arrivalMode);
-        System.out.println("Triage Level: " + triageLevel);
-        System.out.println("Admitted Via ER: " + admittedViaER);
-
+        System.out.println("Admission Date: " + admissionDate);
+        System.out.println("Discharge Date: " + dischargeDate);
+        System.out.println("Room Number: " + roomNumber);
+        System.out.println("Bed Number: " + bedNumber);
+        System.out.println("Admitting Doctor ID: " + admittingDoctorId);
+        System.out.println("Daily Charges: " + dailyCharges);
     }
 
     @Override
     public void displaySummary() {
+
+    }
+
+    public Integer calculateStayDuration(){
+        return Math.toIntExact(ChronoUnit.DAYS.between(admissionDate, dischargeDate));
+    }
+
+    public Double calculateTotalCharges(){
+        return calculateStayDuration() * dailyCharges;
+
+    }
+
+    @Override
+    public void calculateCharges() {
+
+    }
+
+    @Override
+    public void generateBill() {
+
+    }
+
+    @Override
+    public void processPayment(double amount) {
 
     }
 }
