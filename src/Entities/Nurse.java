019e1356ca9972f9b7d1fc5798afdcbfbb95776c
CompanyProfile <alanood94895@gmail.com>
2026-05-12 12:26:07 +0400

    implements Displayable interface

--- src/Entities/Nurse.java
+++ src/Entities/Nurse.java
@@ -1,135 +1,81 @@
 package Entities;
 
+import Behaviour.Displayable;
+import Entities.Patient;
+import Utils.Constants;
+
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
-public class Nurse extends Person {
+public class Nurse extends Person implements Displayable {
 
-    // Nurse specific fields
+    public String getDepartmentId() {
+        return departmentId;
+    }
 
-    private String nurseId;
     private String departmentId;
+
+    public String getShift() {
+        return shift;
+    }
+
     private String shift;
     private String qualification;
-    private List<String> assignedPatients;
+    private List<Patient> assignedPatients;
 
-    // Constructor
+    public Nurse(String id,
+                 String firstName,
+                 String lastName,
+                 LocalDate dateOfBirth,
+                 String gender,
+                 String phoneNumber,
+                 String email,
+                 String address,
+                 String departmentId,
+                 String shift,
+                 String qualification) {
 
-    public Nurse(
-            String id,
-            String firstName,
-            String lastName,
-            LocalDate dateOfBirth,
-            String gender,
-            String phoneNumber,
-            String email,
-            String address,
-            String nurseId,
-            String departmentId,
-            String shift,
-            String qualification,
-            List<String> assignedPatients)
-    {
-
-        super(
-                id,
+        super(id,
                 firstName,
                 lastName,
                 dateOfBirth,
                 gender,
                 phoneNumber,
                 email,
-                address
-        );
+                address);
 
-        this.nurseId = nurseId;
         this.departmentId = departmentId;
         this.shift = shift;
         this.qualification = qualification;
-        this.assignedPatients = new ArrayList<>(assignedPatients);
+
+        this.assignedPatients = new ArrayList<>();
     }
 
-    // Getters and Setters
-
-    public String getNurseId() {
-        return nurseId;
-    }
-
-    public void setNurseId(String nurseId) {
-        this.nurseId = nurseId;
-    }
-
-    public String getDepartmentId() {
-        return departmentId;
-    }
-
-    public void setDepartmentId(String departmentId) {
-        this.departmentId = departmentId;
-    }
-
-    public String getShift() {
-        return shift;
-    }
-
-    public void setShift(String shift) {
-        this.shift = shift;
-    }
-
-    public String getQualification() {
-        return qualification;
-    }
-
-    public void setQualification(String qualification) {
-        this.qualification = qualification;
-    }
-
-    public List<String> getAssignedPatients() {
-        return assignedPatients;
-    }
-
-    public void setAssignedPatients(List<String> assignedPatients) {
-        this.assignedPatients = assignedPatients;
-    }
-
-    // Display Nurse Information
     @Override
     public void displayInfo() {
-
         super.displayInfo();
 
-        System.out.println("Nurse ID: " + nurseId);
         System.out.println("Department ID: " + departmentId);
         System.out.println("Shift: " + shift);
         System.out.println("Qualification: " + qualification);
-        System.out.println("Assigned Patients: " + assignedPatients);
     }
 
-    // Assign Patient
-    public void assignPatient(String patientId) {
+    @Override
+    public void displaySummary() {
 
-        if (!assignedPatients.contains(patientId)) {
-
-            assignedPatients.add(patientId);
-
-            System.out.println("Patient " + patientId + " assigned to Nurse " + nurseId);
-
-        } else {
-
-            System.out.println("Patient already assigned.");
-        }
     }
 
-    // Remove Patient
-    public void removePatient(String patientId) {
+    // Assign patient
+    public void assignPatient(Patient patient) {
+        assignedPatients.add(patient);
+        System.out.println(Constants.PATIENT_ASSIGNED_SUCCESSFULLY);
+    }
 
-        if (assignedPatients.remove(patientId)) {
-
-            System.out.println("Patient " + patientId + " removed from Nurse " + nurseId);
-
-        } else {
-
-            System.out.println("Patient not found.");
-        }
+    // Remove patient
+    public void removePatient(Patient patient) {
+        assignedPatients.remove(patient);
+        System.out.println(Constants.PATIENT_REMOVED_SUCCESSFULLY);
     }
 }
