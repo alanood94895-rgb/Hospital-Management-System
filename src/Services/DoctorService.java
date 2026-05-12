006a15207408e418411225df00bb783f1f1c69b5
CompanyProfile <alanood94895@gmail.com>
2026-05-12 14:45:36 +0400

    Add DoctorService Class

--- src/Services/DoctorService.java
+++ src/Services/DoctorService.java
@@ -5,6 +5,7 @@ import Behaviour.Searchable;
 import Entities.Doctor;
 import Entities.Patient;
 import Utils.Constants;
+import Utils.HelperUtils;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -12,27 +13,18 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
+import static Services.PatientService.getPatients;
+
 public class DoctorService implements Manageable, Searchable {
-
     static Scanner scanner = new Scanner(System.in);
-
     private List<Doctor> doctors = new ArrayList<>();
 
-    PatientService patientService = new PatientService();
-
-    // ADD DOCTOR OBJECT
     public void addDoctor(Doctor doctor) {
-
         doctors.add(doctor);
-
         System.out.println(Constants.DOCTOR_ADDED_SUCCESSFULLY);
     }
 
-    // CREATE NEW DOCTOR
     public Doctor addDoctor() {
-
-        System.out.println("Enter doctor id:");
-        String doctorId = scanner.nextLine();
 
         System.out.println("Enter first name:");
         String firstName = scanner.nextLine();
@@ -48,7 +40,6 @@ public class DoctorService implements Manageable, Searchable {
 
         System.out.println("Enter date of birth (yyyy-MM-dd):");
         String DOB = scanner.nextLine();
-
         LocalDate dateOfBirth = LocalDate.parse(DOB);
 
         System.out.println("Enter email:");
@@ -70,27 +61,25 @@ public class DoctorService implements Manageable, Searchable {
         String departmentId = scanner.nextLine();
 
         System.out.println("Enter consultation fee:");
-        double consultationFee =
-                Double.parseDouble(scanner.nextLine());
+        double consultationFee = Double.parseDouble(scanner.nextLine());
 
         List<String> availableSlots = new ArrayList<>();
 
         System.out.println("Do you want to add available slots? (yes/no)");
-
         String addSlots = scanner.nextLine();
 
         if (addSlots.equalsIgnoreCase("yes")) {
 
             System.out.println("Enter slots separated by commas:");
-
             String slotsInput = scanner.nextLine();
 
-            availableSlots =
-                    Arrays.asList(slotsInput.split(","));
+            availableSlots = Arrays.asList(slotsInput.split(","));
         }
 
+        List<String> assignedPatients = new ArrayList<>();
+
         Doctor doctor = new Doctor(
-                doctorId,
+                HelperUtils.generateId("Dc."),
                 firstName,
                 lastName,
                 dateOfBirth,
@@ -103,18 +92,13 @@ public class DoctorService implements Manageable, Searchable {
                 experienceYears,
                 departmentId,
                 consultationFee
-        );
 
-        doctor.setAvailableSlots(availableSlots);
+        );
 
         return doctor;
     }
 
-    // OVERLOADING 1
-    public void addDoctor(String name,
-                          String specialization,
-                          String phone) {
-
+    public void addDoctor(String name, String specialization, String phone){
         Doctor doctor = new Doctor();
 
         doctor.setFirstName(name);
@@ -124,12 +108,7 @@ public class DoctorService implements Manageable, Searchable {
         doctors.add(doctor);
     }
 
-    // OVERLOADING 2
-    public void addDoctor(String name,
-                          String specialization,
-                          String phone,
-                          double consultationFee) {
-
+    public void addDoctor(String name, String specialization, String phone, double consultationFee){
         Doctor doctor = new Doctor();
 
         doctor.setFirstName(name);
@@ -140,178 +119,123 @@ public class DoctorService implements Manageable, Searchable {
         doctors.add(doctor);
     }
 
-    // EDIT DOCTOR
-    public void editDoctor(String doctorId,
-                           Doctor updatedDoctor) {
 
-        for (Doctor d : doctors) {
-
-            if (d.getId().equals(doctorId)) {
-
+    public void editDoctor(String doctorId, Doctor updatedDoctor){
+        for(Doctor d : doctors){
+            if(d.getId().equals(doctorId)){
                 d.setPhoneNumber(updatedDoctor.getPhoneNumber());
                 d.setEmail(updatedDoctor.getEmail());
                 d.setAddress(updatedDoctor.getAddress());
 
-                System.out.println(
-                        Constants.DOCTOR_UPDATED_SUCCESSFULLY);
-
+                System.out.println(Constants.DOCTOR_UPDATED_SUCCESSFULLY);
                 return;
             }
         }
-
         System.out.println(Constants.DOCTOR_NOT_FOUND);
     }
 
-    // REMOVE DOCTOR
-    public void removeDoctor(String doctorId) {
-
-        for (Doctor d : doctors) {
-
+    public void removeDoctor(String doctorId){
+        for(Doctor d : doctors) {
             if (d.getId().equals(doctorId)) {
-
                 doctors.remove(d);
-
-                System.out.println(
-                        Constants.DOCTOR_REMOVED_SUCCESSFULLY);
-
+                System.out.println(Constants.DOCTOR_REMOVED_SUCCESSFULLY);
                 return;
             }
         }
-
         System.out.println(Constants.DOCTOR_NOT_FOUND);
     }
 
-    // GET DOCTOR BY ID
-    public Doctor getDoctorById(String doctorId) {
-
-        for (Doctor d : doctors) {
-
-            if (d.getId().equals(doctorId)) {
-
+    public Doctor getDoctorById(String doctorId){
+        for(Doctor d : doctors){
+            if(d.getId().equals(doctorId)){
+                d.displayInfo();
                 return d;
             }
         }
-
+        System.out.println(Constants.DOCTOR_NOT_FOUND);
         return null;
     }
 
-    // DISPLAY ALL DOCTORS
-    public void displayDoctors() {
-
-        for (Doctor d : doctors) {
-
+    public void displayDoctors(){
+        for(Doctor d : doctors){
             d.displayInfo();
         }
     }
 
-    // DISPLAY DOCTORS BY SPECIALIZATION
-    public void displayDoctors(String specialization) {
-
-        for (Doctor d : doctors) {
-
-            if (d.getSpecialization()
-                    .equalsIgnoreCase(specialization)) {
-
+    public void displayDoctors(String specialization){
+        for(Doctor d : doctors){
+            if(d.getSpecialization().equalsIgnoreCase(specialization)){
+                d.displayInfo();
+            }
+        }
+    }
+    public void displayDoctors(String departmentId, boolean showAvailableOnly) {
+        for(Doctor d : doctors){
+            if(d.getDepartmentId().equalsIgnoreCase(departmentId) && !d.getAvailableSlots().isEmpty()){
                 d.displayInfo();
             }
         }
     }
 
-    // DISPLAY AVAILABLE DOCTORS
-    public void displayDoctors(String departmentId,
-                               boolean showAvailableOnly) {
-
-        for (Doctor d : doctors) {
-
-            if (d.getDepartmentId()
-                    .equalsIgnoreCase(departmentId)
-                    &&
-                    !d.getAvailableSlots().isEmpty()) {
-
+    public void getAvailableDoctors(){
+        for(Doctor d : doctors){
+            if(!d.getAvailableSlots().isEmpty()){
                 d.displayInfo();
             }
         }
     }
 
-    // AVAILABLE DOCTORS
-    public void getAvailableDoctors() {
+    public void assignPatient(String doctorId, String patientId){
+        for (Doctor doctor : doctors){
 
-        for (Doctor d : doctors) {
-
-            if (!d.getAvailableSlots().isEmpty()) {
-
-                d.displayInfo();
-            }
-        }
-    }
-
-    // ASSIGN PATIENT
-    public void assignPatient(String doctorId,
-                              String patientId) {
-
-        for (Doctor doctor : doctors) {
-
-            if (doctor.getId().equals(doctorId)) {
-
-                for (Patient p : patientService.getPatients()) {
-
-                    if (p.getId().equals(patientId)) {
-
+            for(Patient p : getPatients()){
+                if(doctor.getId().equals(doctorId)){
+                    if(p.getId().equals(patientId)){
                         doctor.getAssignedPatients().add(p);
-
-                        System.out.println(
-                                Constants.PATIENT_ASSIGNED_SUCCESSFULLY
-                        );
-
+                        System.out.println(Constants.PATIENT_ASSIGNED_SUCCESSFULLY);
                         return;
                     }
+                    System.out.println(Constants.PATIENT_NOT_FOUND);
+
+
                 }
-
-                System.out.println(Constants.PATIENT_NOT_FOUND);
-
-                return;
+                System.out.println(Constants.DOCTOR_NOT_FOUND);
             }
+
         }
 
-        System.out.println(Constants.DOCTOR_NOT_FOUND);
+
     }
 
-    // OVERLOADING
-    public void assignPatient(Doctor doctor,
-                              Patient patient) {
-
+    public void  assignPatient(Doctor doctor, Patient patient){
         doctor.getAssignedPatients().add(patient);
     }
 
-    // OVERLOADING
-    public void assignPatient(String doctorId,
-                              List<String> patientIds) {
-
-        for (Doctor doctor : doctors) {
-
-            if (doctor.getId().equals(doctorId)) {
-
-                for (Patient p : patientService.getPatients()) {
-
-                    for (String x : patientIds) {
-
-                        if (p.getId().equals(x)) {
-
+    public void  assignPatient(String doctorId, List<String> patientIds) {
+        for (Doctor doctor : doctors){
+            if(doctor.getId().equals(doctorId)){
+                for(Patient p : getPatients()){
+                    for(String x : patientIds){
+                        if(p.getId().equals(x)){
                             doctor.getAssignedPatients().add(p);
+                            System.out.println(Constants.PATIENT_ASSIGNED_SUCCESSFULLY);
+                        }else{
+                            System.out.println(Constants.PATIENT_NOT_FOUND);
                         }
+
                     }
+
                 }
 
-                System.out.println(
-                        Constants.PATIENT_ASSIGNED_SUCCESSFULLY
-                );
-
-                return;
+            }else{
+                System.out.println(Constants.DOCTOR_NOT_FOUND);
             }
+
+
         }
 
-        System.out.println(Constants.DOCTOR_NOT_FOUND);
     }
+
 
     // INTERFACE METHODS
     @Override
@@ -354,29 +278,22 @@ public class DoctorService implements Manageable, Searchable {
             case 2 -> {
 
                 System.out.print("Enter Doctor ID to edit: ");
-
                 String id = scanner.nextLine().trim();
-
                 Doctor updatedDoctor = addDoctor();
-
                 editDoctor(id, updatedDoctor);
             }
 
             case 3 -> {
 
                 System.out.print("Enter Doctor ID to remove: ");
-
                 String id = scanner.nextLine().trim();
-
                 removeDoctor(id);
             }
 
             case 4 -> {
 
                 System.out.print("Enter Doctor ID to search: ");
-
                 String id = scanner.nextLine().trim();
-
                 Doctor d = getDoctorById(id);
 
                 if (d != null) {
@@ -391,11 +308,8 @@ public class DoctorService implements Manageable, Searchable {
 
             case 5 -> {
 
-                System.out.print(
-                        "Enter specialization to search: ");
-
+                System.out.print("Enter specialization to search: ");
                 String spec = scanner.nextLine().trim();
-
                 displayDoctors(spec);
             }
 
@@ -411,8 +325,7 @@ public class DoctorService implements Manageable, Searchable {
 
             default -> {
 
-                System.out.println(
-                        "Invalid option. Please choose 1-7.");
+                System.out.println("Invalid option. Please choose 1-7.");
             }
         }
 
