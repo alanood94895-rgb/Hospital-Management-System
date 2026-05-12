743c0a434b2cace375b9d58c9fbce31c41fd0ec8
CompanyProfile <alanood94895@gmail.com>
2026-05-12 12:27:42 +0400

    implements Displayable interface

--- src/Entities/Person.java
+++ src/Entities/Person.java
@@ -1,23 +1,29 @@
 package Entities;
 
+import Behaviour.Displayable;
+
 import java.time.LocalDate;
 import java.util.Objects;
 
-public class Person {
-    private String id ;
-    private String firstName ;
+public class Person implements Displayable {
+    private String id;
+    private String firstName;
     private String lastName;
-    private LocalDate dateOfBirth ;
+    private LocalDate dateOfBirth;
     private String gender;
     private String phoneNumber;
     private String email;
     private String address;
 
-
-
-    public Person(String id, String firstName, String lastName, LocalDate dateOfBirth, String gender,
-                  String phoneNumber, String email, String address) {
-
+    //constructor
+    public Person(String id,
+                  String firstName,
+                  String lastName,
+                  LocalDate dateOfBirth,
+                  String gender,
+                  String phoneNumber,
+                  String email,
+                  String address){
 
         this.id = id;
         this.firstName = firstName;
@@ -27,61 +33,19 @@ public class Person {
         this.phoneNumber = phoneNumber;
         this.email = email;
         this.address = address;
-
-        }
+    }
 
     public Person() {
 
     }
 
-    // Getters and Setters
-
-    public String getId() {
-            return id;
-        }
-
-    public void setId(String id) {
-        this.id = id;
+    // getters ans setters
+    public String getAddress() {
+        return address;
     }
 
-    public String getFirstName() {
-        return firstName;
-    }
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public LocalDate getDateOfBirth() {
-        return dateOfBirth;
-    }
-
-    public void setDateOfBirth(LocalDate dateOfBirth) {
-        this.dateOfBirth = dateOfBirth;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
+    public void setAddress(String address) {
+        this.address = address;
     }
 
     public String getEmail() {
@@ -92,31 +56,74 @@ public class Person {
         this.email = email;
     }
 
-    public String getAddress() {
-        return address;
+    public String getPhoneNumber() {
+        return phoneNumber;
     }
 
-    public void setAddress(String address) {
-        this.address = address;
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
     }
 
-    // displayInfo Method
-    public void displayInfo() {
-
-        System.out.println("ID: " + id);
-        System.out.println("First Name: " + firstName);
-        System.out.println("Last Name: " + lastName);
-        System.out.println("Date of Birth: " + dateOfBirth);
-        System.out.println("Gender: " + gender);
-        System.out.println("Phone Number: " + phoneNumber);
-        System.out.println("Email: " + email);
-        System.out.println("Address: " + address);
+    public String getGender() {
+        return gender;
     }
 
-    // toString Method
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void displayInfo(){
+        System.out.println("ID:"+ id);
+        System.out.println("First Name:"+ firstName);
+        System.out.println("Last Name:"+lastName );
+        System.out.println("Date of Birth  :"+dateOfBirth );
+        System.out.println("Gender:"+ gender);
+        System.out.println("Phone Number  :"+ phoneNumber);
+        System.out.println("Email:"+email );
+        System.out.println("Address :"+ address);
+    }
+
+    @Override
+    public void displaySummary() {
+
+    }
+
+
+    //toString Method
     @Override
     public String toString() {
-
         return "Person{" +
                 "id='" + id + '\'' +
                 ", firstName='" + firstName + '\'' +
@@ -129,27 +136,16 @@ public class Person {
                 '}';
     }
 
-    // equals Method
+    // equals method
     @Override
-    public boolean equals(Object obj) {
-            if (obj == null || getClass() != obj.getClass()) return false;
-            Person person = (Person) obj;
-            return Objects.equals(id, person.id)
-                    && Objects.equals(firstName, person.firstName)
-                    && Objects.equals(lastName, person.lastName)
-                    && Objects.equals(dateOfBirth, person.dateOfBirth)
-                    && Objects.equals(gender, person.gender)
-                    && Objects.equals(phoneNumber, person.phoneNumber)
-                    && Objects.equals(email, person.email)
-                    && Objects.equals(address, person.address);
-        }
-
-    // hashCode Method
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return Objects.equals(id, person.id) && Objects.equals(firstName, person.firstName) && Objects.equals(lastName, person.lastName) && Objects.equals(dateOfBirth, person.dateOfBirth) && Objects.equals(gender, person.gender) && Objects.equals(phoneNumber, person.phoneNumber) && Objects.equals(email, person.email) && Objects.equals(address, person.address);
+    }
+    //hashcode method
     @Override
     public int hashCode() {
-            return Objects.hash(id, firstName,
-                    lastName, dateOfBirth, gender,
-                    phoneNumber, email, address);
-        }
+        return Objects.hash(id);
+    }
 }
-
