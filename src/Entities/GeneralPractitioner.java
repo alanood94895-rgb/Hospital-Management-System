2d4d393b1aeae712cac46ca02db76df08bdf237e
CompanyProfile <alanood94895@gmail.com>
2026-05-13 09:31:13 +0400

    Add GeneralPractitioner Class

--- src/Entities/GeneralPractitioner.java
+++ src/Entities/GeneralPractitioner.java
@@ -1,4 +1,5 @@
 package Entities;
+
 import Behaviour.Displayable;
 
 import java.time.LocalDate;
@@ -9,37 +10,14 @@ public class GeneralPractitioner extends Doctor implements Displayable {
     Boolean vaccinationCertified;
 
 
-    public GeneralPractitioner(String id,
-                               String firstName,
-                               String lastName,
-                               LocalDate dateOfBirth,
-                               String gender,
-                               String phoneNumber,
-                               String email,
-                               String address,
-                               String specialization,
-                               String qualification,
-                               Integer experienceYears,
-                               String departmentId,
-                               double consultationFee,
-                               Boolean walkinAvailable,
-                               Boolean homeVisitAvailable,
+    public GeneralPractitioner(String id, String firstName, String lastName, LocalDate dateOfBirth,
+                               String gender, String phoneNumber, String email, String address,
+                               String specialization, String qualification, Integer experienceYears,
+                               String departmentId, double consultationFee,Boolean walkinAvailable, Boolean homeVisitAvailable,
                                Boolean vaccinationCertified) {
 
-        super(id,
-                firstName,
-                lastName,
-                dateOfBirth,
-                gender,
-                phoneNumber,
-                email,
-                address,
-                specialization,
-                qualification,
-                experienceYears,
-                departmentId,
-                consultationFee);
 
+        super(id, firstName, lastName, dateOfBirth, gender, phoneNumber, email, address, specialization, qualification, experienceYears, departmentId, consultationFee);
         this.walkinAvailable = walkinAvailable;
         this.homeVisitAvailable = homeVisitAvailable;
         this.vaccinationCertified = vaccinationCertified;
@@ -48,10 +26,20 @@ public class GeneralPractitioner extends Doctor implements Displayable {
     @Override
     public void displayInfo() {
         super.displayInfo();
+
+    }
+
+    @Override
+    public void displaySummary() {
+        System.out.println("===== Doctor Summary =====");
+        System.out.println("Doctor ID: " + getId());
+        System.out.println("Name: " + getFirstName() + " " + getLastName());
         System.out.println("Walk in Available: " + walkinAvailable);
         System.out.println("home Visit Available: " + homeVisitAvailable);
         System.out.println("Vaccination Certified: " + walkinAvailable);
     }
+
+
 
     public void scheduleHomeVisit(){
         if(homeVisitAvailable){
