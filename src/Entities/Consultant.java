d46750114f0d649bf20b0584ca35baf52eb93c84
CompanyProfile <alanood94895@gmail.com>
2026-05-13 09:27:34 +0400

    Dislplay Summary Method to Consultant Class

--- src/Entities/Consultant.java
+++ src/Entities/Consultant.java
@@ -20,7 +20,9 @@ public class Consultant extends Doctor implements Displayable {
                       LocalDate dateOfBirth,
                       String gender,
                       String phoneNumber,
-                      String email, String address, String specialization, String qualification,
+                      String email,
+                      String address,
+                      String specialization, String qualification,
                       Integer experienceYears, String departmentId, double consultationFee
     ) {
         super(id, firstName, lastName, dateOfBirth, gender, phoneNumber, email, address, specialization, qualification, experienceYears, departmentId, consultationFee);
